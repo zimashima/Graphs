fcc4d33c4f010f13f984faf9591fd784e795e748
Mashima <52222646+zimashima@users.noreply.github.com>
2020-05-12 09:18:07 -0400

    first push

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,19 +13,25 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertext does not exist in this graph")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        if vertex_id in self.vertices:
+            return self.vertices[vertex_id]
+        else:
+            print('FAIL')
 
     def bft(self, starting_vertex):
         """
