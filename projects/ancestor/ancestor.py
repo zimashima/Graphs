c7d1f012b6303afaeb956d76bef832e2c0fd1e9a
Mashima <52222646+zimashima@users.noreply.github.com>
2020-05-13 08:17:18 -0400

    some progress

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,11 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    vertices = {}
+
+
+
+    return earliest
+
+
+ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+print(earliest_ancestor(t))
